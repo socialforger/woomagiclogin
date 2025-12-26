2a321113686765c42e3a6580960666ca05cc7ffc
Socialforger <socialforger@gmail.com>
2025-12-26 04:34:12 +0100

    Create woomagiclogin.php

---
+++ woomagiclogin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Woo Magic Login
+ * Description: Registrazione e accesso con magic link per WooCommerce.
+ * Version: 0.3.0
+ * Author: Social Market
+ * Text Domain: woomagiclogin
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'WML_PLUGIN_VERSION', '0.3.0' );
+define( 'WML_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WML_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+require_once WML_PLUGIN_DIR . 'includes/class-woomagiclogin-plugin.php';
+
+function wml_init_plugin() {
+    \Woomagiclogin\Plugin::instance();
+}
+add_action( 'plugins_loaded', 'wml_init_plugin' );
