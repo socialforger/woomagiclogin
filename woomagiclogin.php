076e5fe9cdede59b80bda45742a9859fc3948407
Socialforger <socialforger@gmail.com>
2025-12-26 04:46:36 +0100

    Update woomagiclogin.php

--- woomagiclogin.php
+++ woomagiclogin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Woo Magic Login
  * Description: Registrazione e accesso con magic link per WooCommerce.
  * Version: 0.3.0
- * Author: Social Market
+ * Author: Socialforger
  * Text Domain: woomagiclogin
  * Domain Path: /languages
  */
@@ -16,8 +16,14 @@ define( 'WML_PLUGIN_VERSION', '0.3.0' );
 define( 'WML_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WML_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+/**
+ * Carica il plugin principale
+ */
 require_once WML_PLUGIN_DIR . 'includes/class-woomagiclogin-plugin.php';
 
+/**
+ * Inizializza il plugin
+ */
 function wml_init_plugin() {
     \Woomagiclogin\Plugin::instance();
 }
